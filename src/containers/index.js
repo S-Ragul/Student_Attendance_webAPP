28b78b71560d6550187e1dbed8ab494c19cebcfd
S-Ragul <113603933+S-Ragul@users.noreply.github.com>
2023-08-26 22:06:49 +0530

    first commit

---
+++ src/containers/index.js
@@ -0,0 +1,4 @@
+export { default as Blog} from './blog/Blog';
+export { default as Features} from './features/Features';
+export { default as Header} from './header/Header';
+export { default as WhatStd_att} from './whatStd_att/WhatStd_att';
---
+++ src/containers/whatStd_att/WhatStd_att.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './whatStd_att.css';
+
+const WhatStd_att = () => {
+  return (
+    <div>WhatStd_att</div>
+  )
+}
+
+export default WhatStd_att
