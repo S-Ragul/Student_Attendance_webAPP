796553b305d8b0af96395271fc5c92fc2f0441b6
S-Ragul <113603933+S-Ragul@users.noreply.github.com>
2023-08-27 17:14:34 +0530

    frist commmit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
 import './navbar.css';
-import {RiMenu3Line, RicloseLine } from 'react-icons/ri';
-
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 const Navbar = () => {
   return (
-
-
+    <div className="Navbar__container">
       <div className="Student__list-links">
         <div className="Student__list-links_logo">
-         <div className="student__links_container">
-           <p><a href='#home'>Home</a> </p>
-           <p><a href='#alumini'>Alumini</a> </p>
-           <p><a href='#examinations'>Examinations</a> </p>
-           <p><a href='#admission'>Admission</a> </p>
-         </div>
-       </div>
-       <div className="student__links_sign">
-        <p>Sign In</p>
-        <button type='button'> Sign Up</button>
-       </div>
+          <div className="student__links_container">
+            <p><a href='#home'>Home</a></p>
+            <p><a href='#alumini'>Alumini</a></p>
+            <p><a href='#examinations'>Examinations</a></p>
+            <p><a href='#admission'>Admission</a></p>
+          </div>
+        </div>
+        <div className="student__links_sign">
+          <p><a>Sign In</a></p>
+          <button type='button'>Sign Up</button>
+        </div>
       </div>
-    
+    </div>
+  );
+};
 
-    
-  )
-}
-
-export default Navbar
+export default Navbar;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,3 @@
 export { default as Blog} from './blog/Blog';
-export { default as Features} from './features/Features';
 export { default as Header} from './header/Header';
 export { default as WhatStd_att} from './whatStd_att/WhatStd_att';
